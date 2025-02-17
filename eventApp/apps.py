8d0dc3e5624944e48ca7db1158bdc96cb7308949
Neralis <red4ct@yandex.ru>
2025-02-17 10:36:02 +0300

    *Исправлено добавление события через изменение получения default пользователя

--- eventApp/apps.py
+++ eventApp/apps.py
@@ -3,25 +3,23 @@ from django.db.models.signals import post_migrate
 from django.utils.timezone import now
 
 
-
-
 class EventappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'eventApp'
 
     def ready(self):
         # Подключение сигнала для создания пользователя после миграции
-        post_migrate.connect(get_default_organizer, sender=self)
+        post_migrate.connect(create_default_organizer, sender=self)
 
 
-def get_default_organizer(sender, **kwargs):
+def create_default_organizer(sender, **kwargs):
 
     from userApp.models import CustomUser
 
-    user, _ = CustomUser.objects.get_or_create(
+    CustomUser.objects.get_or_create(
         username='default_remove_user',
         email='remove_user@gmail.com',
         date_birthday=now(),
         phone='+12345678911'
     )
-    return user
+
