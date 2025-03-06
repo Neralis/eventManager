5b3996bcdcddc6a87a2b6a5b65af9eb2a758690d
Barsyk <georgiy.nikitin.2005@mail.ru>
2025-03-06 21:00:38 +0300

    Исправил подключение celery

--- eventManager/settings.py
+++ eventManager/settings.py
@@ -153,7 +153,7 @@ EMAIL_USE_SSL = True
 EMAIL_HOST_USER = 'ner4corp@gmail.com'
 EMAIL_HOST_PASSWORD = 'oimy thbj nfxn lfhx'
 
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_BROKER_URL = 'redis://redis:6379/0'
+CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
--- participantApp/views.py
+++ participantApp/views.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, ListView, DeleteView
